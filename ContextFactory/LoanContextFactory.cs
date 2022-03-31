87d50c0504b388b2f94dcc49a1751b4e9b229f2f
onyekachii <ugwu_kennedy@icloud.com>
2022-03-31 11:07:37 +0100

    context factory setup

---
+++ ContextFactory/LoanContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace loanApp.ContextFactory
+{
+    public class LoanContextFactory : IDesignTimeDbContextFactory<LoanContext>
+    {
+        public LoanContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<LoanContext>()
+                .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+                b => b.MigrationsAssembly("loanApp"));
+
+            return new LoanContext(builder.Options);
+        }
+    }
+}
